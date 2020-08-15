24facdbaf3c31290e74827cf937545b1fccf0eda
Jonathan Kemper <jonkemper@gmail.com>
2020-08-15 18:59:30 -0400

    End of tutorial update

--- app.js
+++ app.js
@@ -3,16 +3,55 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var users = require('./routes/users');
 var generate_uid = require('./routes/generate_uid');
+var customer = require('./routes/customer');
 
 var app = express();
+
+let reporter = function (type, ...rest) {
+  // remote reporter logic goes here
+};
+
+/* handle an uncaught exception & exit the process */
+process.on('uncaughtException', function (err) {
+  console.error(new Date().toUTCString() + ' uncaughtException:', err.message);
+  console.error(err.stack);
+
+  reporter(
+    'uncaughtException',
+    new Date().toUTCString(),
+    err.message,
+    err.stack
+  );
+
+  process.exit(1);
+});
+
+/* handle an unhandled promise rejection */
+process.on('unhandledRejection', function (reason, promise) {
+  console.error('unhandled rejection:', reason.message || reason);
+
+  reporter(
+    'uncaughtException',
+    new Date().toUTCString(),
+    reason.message || reason
+  );
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use('/api/v1/customer', customer);
 app.use('/api/v1/generate_uid', generate_uid);
-app.use('/api/v1/users', users);
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 module.exports = app;
